8cee89f12e3fc436c53cecd9f850cb6a1af1e1c0
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-12-12 14:31:33 +0530

    react1212

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+
 import './App.css';
 import './index.css';
 import Cards from './table/home'
@@ -15,20 +15,7 @@ import Accordion from './dec4cards/index'
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      
       <FormComp/>
       <Cards/>
       <Productcards/>
